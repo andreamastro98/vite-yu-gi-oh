a5e500c7af12c56a0f51e1b90367aeead8c053e0
andreamastro98 <andreamastrovito98@gmail.com>
2023-05-05 16:10:31 +0200

    chiamata di 20 carte dall'api fatta

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,41 @@
 <script >
 
+import axios from 'axios'
+import {store} from './store'
+
+export default{
+  name:'app',
+  components:{
+
+  },
+  data(){
+    return{
+      store
+    }    
+  },
+  created(){
+    this.chiamataApi()
+
+  },
+  methods:{
+    chiamataApi(){
+      axios.get(' https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+      .then((res)=>{
+
+        // associo a datiApi il le 20 carte restituite dall'api
+        const datiApi = res.data;
+
+        //associo allárray in store.js la const datiApi
+        this.store.ArrayCarte = datiApi
+
+        console.log(this.store.ArrayCarte)
+        }
+      
+      )
+    }
+  }
+}
+
 </script>
 
 <template>
